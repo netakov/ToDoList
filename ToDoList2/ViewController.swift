2f8924437a59a7b5df6a4a11c8850c714ea8a9cc
netakov <nikolajnetakov@kolya-netakov.local>
2014-10-16 00:21:53 +0400

    Initial Commit

---
+++ ToDoList2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ToDoList2
+//
+//  Created by netakov on 16.10.14.
+//  Copyright (c) 2014 kolya netakov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
